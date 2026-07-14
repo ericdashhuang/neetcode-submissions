db815c495f756240dcc774e5c9693ca09187b78a
Eric Huang <69663998+ericdashhuang@users.noreply.github.com>
2026-07-14 13:30:17 -0700

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,41 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+        
+        count1 = [0] * 26
+        count2 = [0] * 26
+        matches = 0
+        for i in s1:
+            count1[ord(i) - ord("a")] += 1
+
+        for i in range(len(s1)):
+            count2[ord(s2[i]) - ord("a")] += 1
+
+        for i in range(26):
+            if count1[i] == count2[i]:
+                matches += 1
+        
+        l = 0
+
+        if matches == 26: return True
+
+        for r in range(len(s1), len(s2)):
+
+            count2[ord(s2[r]) - ord("a")] += 1
+            if count2[ord(s2[r])-ord("a")] == count1[ord(s2[r])-ord("a")]: 
+                matches += 1
+            elif count2[ord(s2[r])-ord("a")] == count1[ord(s2[r])-ord("a")] +1:
+                matches -= 1
+
+            count2[ord(s2[l]) - ord("a")] -= 1
+            if count2[ord(s2[l])-ord("a")] == count1[ord(s2[l])-ord("a")]: matches += 1
+            elif count2[ord(s2[l])-ord("a")] == count1[ord(s2[l])-ord("a")] - 1:
+                matches -= 1
+            l += 1
+            
+            if matches == 26: return True
+        return False
+
+
+
+
